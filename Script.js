3c2def35cdd19c6a671e25414d5f7453ecb0f97a
andygowrley <38193022+andygowrley@users.noreply.github.com>
2025-11-04 22:31:00 +0000

    Delete Script.js

--- Script.js
+++
@@ -1,6 +0,0 @@
-// Simple theme toggler
-const toggleButton = document.getElementById('toggleButton');
-
-toggleButton.addEventListener('click', () => {
-  document.body.classList.toggle('dark-theme');
-});
